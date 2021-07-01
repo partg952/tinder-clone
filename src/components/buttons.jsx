e40899312fab69a297f6159fdb0868e3928cf87d
parth <dankparth@gmail.com>
2021-07-01 23:09:37 +0530

    first commit

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -14,7 +14,7 @@ export default function Buttons() {
         display: flex;
         align-items: center;
         justify-content: center;
-        margin: 10px;
+        /* margin: 10px; */
         height: 60px;
         width: 60px;
         box-shadow: 2px 2px 5px rgba(0,0,0,0.5), 10px 10px 15px rgba(0,0,0,0.5), -1px -1px 30px rgba(0,0,0,0.2);
